4e097f25d6f2fca8464d2cfdf938d588a9c29f6f
Egha Dhiwangkara <Egha@Macbooks-MacBook-Pro.local>
2020-12-16 13:43:01 +0700

    crud

---
+++ Controllers/controller-band.js
@@ -0,0 +1,80 @@
+const { Band } = require('../models')
+
+class BandController{
+    static showBand(req, res){
+        Band.findAll()
+        .then(data =>{
+            res.render('band', {
+                data
+            })
+        })
+        .catch(err =>{
+            res.send(err)
+        })
+    }
+
+    static addBand(req, res){
+        res.render('addBand')
+    }
+
+    static addBandPost(req, res){
+        let obj = {
+            band_name: req.body.band_name,
+            band_origin: req.body.band_origin,
+            band_member: req.body.band_member
+        }
+
+        Band.create(obj)
+        .then(data =>{
+            res.redirect('/band')
+        })
+        .catch(err =>{
+            res.send(err)
+        })
+    }
+
+    static editBand(req, res){
+        let id = +req.params.id
+
+        Band.findByPk(id)
+        .then(data =>{
+            res.render('editBand', {
+                data
+            })
+        })
+        .catch(err =>{
+            res.send(err)
+        })
+    }
+
+    static editBandPost(req, res){
+        let id = +req.params.id
+        let obj = {
+            band_name: req.body.band_name,
+            band_origin: req.body.band_origin,
+            band_member: req.body.band_member
+        }
+
+        Band.update(obj, {where: {id}})
+        .then(data =>{
+            res.redirect('/band')
+        })
+        .catch(err => {
+            res.send(err)
+        })
+    }
+
+    static deleteBand(req, res){
+        let id = +req.params.id
+
+        Band.destroy({where:{id}})
+        .then(data =>{
+            res.redirect('/band')
+        })
+        .catch(err =>{
+            res.send(err)
+        })
+    }
+}
+
+module.exports = BandController
